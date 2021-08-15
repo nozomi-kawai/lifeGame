bab4c9de38eedc3c82ad2ffd7c708b5a892e985a
n.kawai <nozomi.kawai@itage.co.jp>
2021-08-15 20:11:17 +0900

    プロジェクト ファイルを追加します。

---
+++ LifeGame/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace LifeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ImageUpdateTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(820, 477);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ImageUpdateTimer
+            // 
+            this.ImageUpdateTimer.Interval = 300;
+            this.ImageUpdateTimer.Tick += new System.EventHandler(this.ImageUpdateTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 475);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer ImageUpdateTimer;
+    }
+}
+
---
+++ LifeGame/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeGame
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bmpUpdate;
+        private int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ImageCreater();
+            this.ImageUpdateTimer.Start();
+        }
+
+        private void ImageCreater()
+        {
+            this.bmpUpdate = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            Graphics g = Graphics.FromImage(bmpUpdate);
+
+            if (this.count % 3 == 0)
+            {
+                g.FillRectangle(Brushes.White, 10, 20, bmpUpdate.Width - 20, bmpUpdate.Height - 40);
+            }
+            else if (this.count % 3 == 1)
+            {
+                g.FillRectangle(Brushes.Blue, 10, 20, bmpUpdate.Width - 20, bmpUpdate.Height - 40);
+            }
+            else if (this.count % 3 == 2)
+            {
+                g.FillRectangle(Brushes.Pink, 10, 20, bmpUpdate.Width - 20, bmpUpdate.Height - 40);
+            }
+            else
+            {
+                g.FillRectangle(Brushes.Black, 10, 20, bmpUpdate.Width - 20, bmpUpdate.Height - 40);
+            }
+
+            this.pictureBox1.Image = this.bmpUpdate;
+            this.count++;
+
+        }
+
+        private void ImageUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            ImageCreater();
+        }
+    }
+}
