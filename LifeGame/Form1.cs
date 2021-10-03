1329e0251863c8dfc36b9cea9e2087727cf8413a
n.kawai <nozomi.kawai@itage.co.jp>
2021-10-04 01:28:40 +0900

    Fromからの呼び出しを修正

--- LifeGame/Form1.cs
+++ LifeGame/Form1.cs
@@ -15,21 +15,20 @@ namespace LifeGame
     public partial class Form1 : Form
     {
         // セルの数
-        private static int cellCountWidth = 20;
-        private static int cellCountHeight = 20;
+        private static int cellCountWidth = 30;
+        private static int cellCountHeight = 30;
         // セルのサイズ
         // TODO セルのサイズは正方形がいい
-        private static int cellWidth = 20;
-        private static int cellHeight = 20;
+        private static int cellWidth = 10;
+        private static int cellHeight = 10;
 
         private static LifeGame lifeGame;
 
-        private static bool debugMode = false;
+        private static bool debugMode = true;
 
         public Form1()
         {           
             InitializeComponent();
-
             lifeGame = new LifeGame(cellCountWidth, cellCountHeight, cellWidth, cellHeight, debugMode);
             this.pictureBox1.Image = lifeGame.Init();
             // タイマースタート
